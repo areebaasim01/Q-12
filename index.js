5ee226b8737015b5ee153f7c8ddd71eae92c65e7
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-14 15:58:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// 12.	Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ["Dua", "Sehar", "Simra", "Ardil", "fareeha"];
+let message = "Hello, i am inviting you to for my birthday celebration";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
+console.log(names[4] + " " + message);
---
+++ index.ts
@@ -0,0 +1,12 @@
+// 12.	Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+
+let names: string[] = ["Dua", "Sehar", "Simra", "Ardil", "fareeha"];
+let message: string =  "Hello, i am inviting you to for my birthday celebration";
+
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
+console.log(names[4] + " " + message);
